eb6eb5ccef9f5c3cc25fbb0f3845b2216da334c8
Mario Guadalupe <mariogr93@gmail.com>
2022-01-12 23:02:04 -0400

    Modified tab routes

--- src/app/navigation/navtabs/navtabs.component.ts
+++ src/app/navigation/navtabs/navtabs.component.ts
@@ -3,13 +3,38 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-navtabs',
   templateUrl: './navtabs.component.html',
-  styleUrls: ['./navtabs.component.css']
+  styleUrls: ['./navtabs.component.css'],
 })
 export class NavtabsComponent implements OnInit {
+  tabLinks: any[] = [
+    {
+      name: 'Home',
+      route: '/',
+    },
+    {
+      name: 'Accommodations',
+      route: '/accommodation',
+    },
+    {
+      name: 'Food and Beberage',
+      route: 'foodandbeverage',
+    },
+    {
+      name: 'Celebrations',
+      route: 'celebrations',
+    },
+    {
+      name: 'Lifestyle',
+      route: 'lifestyle',
+    },
+    {
+      name: 'Offers',
+      route: 'offers',
+    },
+  ];
 
-  constructor() { }
+  activeLink = this.tabLinks[0].name;
+  constructor() {}
 
-  ngOnInit(): void {
-  }
-
+  ngOnInit(): void {}
 }
